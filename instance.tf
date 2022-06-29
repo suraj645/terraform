158664a089428c3cb09b56f628366379ee48ffbd
suraj <surajdk776@gmail.com>
2022-06-29 10:10:52 -0600

    changed

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myweb" {
   ami           = "ami-02d1e544b84bf7502 "
   instance_type = "t2.micro"
-  key_name = "ohio.pem"
+  key_name = "ohio"
   tags = {
     Name = "demo"
   }
