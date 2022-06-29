ff48d8ebc75d54043d1c5f27f11d909a67c6677a
suraj <surajdk776@gmail.com>
2022-06-29 10:22:29 -0600

    pem removes

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myweb" {
   ami           = "ami-07251f912d2a831a3"
   instance_type = "t2.micro"
-  key_name = "ohio.pem"
+  key_name = "ohio"
    tags = {
     Name = "demo"
   }
